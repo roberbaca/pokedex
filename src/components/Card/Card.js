fe9c14a70d4e19f07f215bcef9120764ddca8b1d
Roberto Baca <roberto.nicolas.baca@gmail.com>
2022-04-30 12:19:49 -0300

    Modifique el formato de la respuesta que obtenemos de la api. Ahora la lista de pokemons esta formada por un array de objetos. Configure los modals (pop ups) con la info detallada de cada pokemon

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,68 +3,121 @@ import '../../styles/components/Card.css';
 import '../../styles/Global.css';
 //import axiosInstance from "../utils/axiosInstance";
 import axios from "axios";
+import Modal from '../Modal/Modal'
 
 
 const Card = () => {
 
   const baseURL= "https://pokeapi.co/api/v2/";
 
-  const[allPokemons, setAllPokemons] = useState([])
+  const pokemon = [];
+  const pokeDescription = [];
+  const pokeDescriptionEn = [];
   const [pokeList, setPokeList] = useState([]);
+  const [isOpen, setIsOpen] = useState(false)
+  const [pokeModal, setPokeModal] = useState("");  
 
 
   const getAllPokemons = async () => {
     try {
       const response = await axios.get(baseURL + 'pokemon?limit=20');
-      const data = response.data.results;
-      //console.log(data);
-      
+      const data = response.data.results;      
+    
       for (let i = 0; i < data.length; i++){
-        allPokemons[i] = await axios.get(baseURL + `pokemon/${data[i].name}`);        
-        pokeList[i] = allPokemons[i].data;
-        setPokeList(pokeList);
-      }
-      
-      console.log("LISTA", pokeList);
+        pokemon[i] = await axios.get(baseURL + `pokemon/${data[i].name}`);        
+        pokeDescription[i] = await axios.get(pokemon[i].data.species.url);          
+        pokeDescriptionEn[i] = pokeDescription[i].data.flavor_text_entries.find(d => d.language.name === "en"); // buscamos la descripcion en ingles
 
-    } catch(error){
+        let pokemonData = {
+          id: pokemon[i].data.id,
+          name: pokemon[i].data.name,          
+          img: pokemon[i].data.sprites.front_default,
+          types: [pokemon[i].data.types[0].type.name, pokemon[i].data.types[1] ? pokemon[i].data.types[1].type.name : ""], // algunos pokemon tiene un solo tipo otros 2       
+          stats: {
+            height: pokemon[i].data.height,
+            weight: pokemon[i].data.weight,
+            hp: pokemon[i].data.stats[0].base_stat,            
+            atk: pokemon[i].data.stats[1].base_stat,
+            def: pokemon[i].data.stats[2].base_stat,
+            spAtk: pokemon[i].data.stats[3].base_stat,
+            spDef: pokemon[i].data.stats[4].base_stat,
+            speed: pokemon[i].data.stats[5].base_stat,
+          },                  
+          description: pokeDescriptionEn[i].flavor_text // damos formato al texto de la descripcion del pokemon
+            .replace(/u'\f'/, /u'\n'/)
+            .replace(/\u00AD/g, '')
+            .replace(/\u000C/g, ' ')
+            .replace(/u' -\n'/, ' - ')
+            .replace(/u'-\n'/, '-')
+            .replace(/(\r\n|\n|\r)/gm, ' ')               
+        }       
+        pokeList[i] = pokemonData;        
+        setPokeList(pokeList);
+      }      
+      console.log("LISTA", pokeList);   // para debug
+
+    } catch(error){      
       console.log(error);
     }
   }
  
-  //const [offset, setOffset] = useState(10); 
 
- 
+  const handleClickModal = (e) => {
+    setIsOpen(true);
+    let id = e.target.value;
+    console.log("hiciste click en pokemon #", id);
+    console.log(pokeList[id-1]);
+    setPokeModal(pokeList[id-1]);
+  }
 
 
-  useEffect( () => {
-    //getPokeList();         
+  useEffect( () => {          
     getAllPokemons();
   }, [])
 
   return (
     <div className='container'>     
-      { pokeList.map( (p , index) =>                 
-        <div className={`card ${p.types[0].type.name}`} key={index}>     
-          <div className='card__pokenumbercontainer'>
-            <p className='card__pokenumber'># {p.id}</p>
-          </div>    
-          <div className='card__pokeimgcontainer'>
-            <img src={p.sprites.front_default} alt="pokemonImg" className='card__pokeimg' />
-          </div>
-          <div className='card__pokename'>
-            <h2>{p.name}</h2>
-          </div>
-          <div className="card__poketypecontainer">
-            <p className={`card__poketype--${p.types[0].type.name}`}>{p.types[0].type.name}</p>
-            {p.types[1] && <p className={`card__poketype--${p.types[1]?.type.name}`}>{p.types[1]?.type.name}</p>}
-          </div> 
-          <div className='card_buttoncontainer'>
-            <button className='card__button'>Info</button>
-          </div>
-          
-        </div>        
+      { pokeList.map( (p , index) =>     
+       <div className={`card ${p.types[0]}`} key={index}>     
+        <div className='card__pokenumbercontainer'>
+          <p className='card__pokenumber'># {p.id}</p>
+        </div>    
+        <div className='card__pokeimgcontainer'>
+          <img src={p.img} alt="pokemonImg" className='card__pokeimg' />
+        </div>
+        <div className='card__pokename'>
+          <h2>{p.name}</h2>
+        </div>
+        <div className="card__poketypecontainer">
+          <p className={`card__poketype--${p.types[0]}`}>{p.types[0]}</p>
+          {p.types[1] && <p className={`card__poketype--${p.types[1]}`}>{p.types[1]}</p>}
+        </div> 
+        <div className='card_buttoncontainer'>
+          <button className='card__button' onClick={handleClickModal} value={p.id}>Info</button>
+        </div>       
+      </div>                  
+       
       )}
+
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        <div>
+          <p>{pokeModal.name}</p>
+          <img src={pokeModal.img} alt="pokemonImg" className='card__pokeimg' />         
+          <p>Height: {pokeModal.stats.height}</p>
+          <p>Weight: {pokeModal.stats.weight}</p>
+          <p>HP: {pokeModal.stats.hp}</p>
+          <p>Attack: {pokeModal.stats.atk}</p>
+          <p>Defense: {pokeModal.stats.def}</p>
+          <p>Sp. Attack: {pokeModal.stats.spAtk}</p>
+          <p>Sp. Deffense: {pokeModal.stats.spDef}</p>
+          <p>Speed: {pokeModal.stats.speed}</p>
+          <p>{pokeModal.description}</p>
+
+        </div>
+
+       
+         
+      </Modal>
     </div>
  
   )
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,67 @@
 import React from 'react'
+import ReactDom from 'react-dom'
+import '../../styles/components/Modal.css';
+import '../../styles/Global.css';
 
-const Modal = () => {
-  return (
-    <div>Modal</div>
-  )
+const MODAL_STYLES = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#FFF',
+  padding: '50px',
+  zIndex: 1000
 }
 
-export default Modal
+const OVERLAY_STYLES = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, .7)',
+  zIndex: 1000
+}
+
+export default function Modal({ open, children, onClose }) {
+  if (!open) return null
+
+  return ReactDom.createPortal(
+    <>
+      <div style={OVERLAY_STYLES} />
+      <div className='modal__card'>
+        <div className='modal__header'>
+          <h2 className='modal__title'>Info</h2>
+          <button onClick={onClose} className='modal__button'>x</button>
+        </div>
+
+        <div className='modal__description'>
+          
+        </div>
+        {/* </div>
+            <p>HT</p>
+            <p>WT</p>
+        <div>
+
+        </div>
+        <div className='modal__statistics'>
+          <div>
+            <p>Attack</p>
+            <p>Defense</p>
+            <p>Sp. Atk</p>
+            <p>Sp. Def</p>
+            <p>Sp. Speed</p>
+          </div> */}
+
+    
+        
+        
+
+
+
+        {children}
+      </div>
+    </>,
+    document.getElementById('portal')
+  )
+}
